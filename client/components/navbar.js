1667abf25773649c274d543cbaa81d87e5f25406
Thomas Bunch <17713462+Gumgooki@users.noreply.github.com>
2021-02-10 08:27:57 -0600

    experimenting with colors; i like the cards so far; everything else is hit or miss

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom'
 import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
+  <div className="navBlock">
     <h1>DoppleGram</h1>
     <nav>
       {isLoggedIn ? (
@@ -27,7 +27,6 @@ const Navbar = ({handleClick, isLoggedIn}) => (
         </div>
       )}
     </nav>
-    <hr />
   </div>
 )
 
