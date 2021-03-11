6e0ad9c791db3386147d4a775bae2190a171ed9b
Thomas Bunch <17713462+Gumgooki@users.noreply.github.com>
2021-03-11 09:03:26 -0600

    created the front-end form to change password; will need to go back and place the correct redux function

--- client/components/account.js
+++ client/components/account.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
+
+
+// VALIDATION FUNCTION
+
+const authFormValidation = (credentials) => {
+  let isValid = true
+  if(!credentials.length || credentials === ""){
+    isValid = false
+  }
+  return isValid
+}
+
 
 /**
  * COMPONENT
@@ -9,11 +21,42 @@ import {connect} from 'react-redux'
 export const Account = props => {
 
   const {userName} = props
+  const [state, setState] = useState({
+    userName: '',
+    password: ''
+  })
+
+
+  //will probably break these components out into seperate form components (one for username and one for pasword)
+  const handleSubmit = (evt) => {
+    evt.preventDefault()
+    if(authFormValidation(state.password)){
+      // need the correct redux function
+      // props.sendAuth(state.password)
+    }else{
+      console.error('please fill out the field')
+    }
+  }
+
+
+  const handleChange = evt => {
+    setState({...state, [evt.target.name]: evt.target.value})
+  }
 
   return (
     <div className='accountDiv'>
       <h3>Welcome, {userName}</h3>
-      <p>Will eventually put ways to change account info here</p>
+      <p>Will eventually put ways to change Usename or Password here</p>
+      <div>
+        <h3>Change Password</h3>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="password">
+            <small>Password</small>
+          </label>
+          <input name="password" type="password" value={state.password} onChange={handleChange}/>
+          <button type="submit">Change Password</button>
+        </form>
+      </div>
     </div>
   )
 }
