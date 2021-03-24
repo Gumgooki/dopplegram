d5dd255e971297692bb43792437e3759a0e0a049
Thomas Bunch <17713462+Gumgooki@users.noreply.github.com>
2021-03-24 17:16:40 -0500

    have succeeded in creating password validation; they at least need to match

--- client/components/changePassword.js
+++ client/components/changePassword.js
@@ -5,28 +5,39 @@ import {editAccount} from '../store'
 
 //VALIDATION FUNCTION: will need to make sure there is a confirmation in order to turn change the passwords
 
-const passwordValidation = (password, confirmPassword) => {
-  let isValid = true
-  if(!password.length || password === ''){
-    isValid = false
-  }
-  if(!confirmPassword.length || confirmPassword === ''){
-    isValid = false
-  }
-
-
-}
-
 export const ChangePassword = props => {
   const {id} = props
   const [state, setState] = useState({
     password: '',
-    confirmPassword: ''
+    confirmPassword: '',
+    errors: {}
   })
+
+  const passwordValidation = (password, confirmPassword) => {
+    let isValid = true
+    let errors = {}
+    if(!password.length || password === ''){
+      isValid = false
+    }
+    if(!confirmPassword.length || confirmPassword === ''){
+      isValid = false
+    }
+    if(typeof password !== undefined && typeof confirmPassword !== undefined){
+      if(password !== confirmPassword){
+        isValid = false
+        errors['password']="Passwords don't match"
+      }
+    }
+    console.log(errors)
+    setState({...state, errors: errors})
+    return isValid
+  }
 
   const handleSubmit = (evt) => {
     evt.preventDefault()
-    props.editAccount(state, id)
+    if(passwordValidation(state.password, state.confirmPassword)){
+      props.editAccount(state, id)
+    }
   }
 
   const handleChange = evt => {
@@ -43,6 +54,14 @@ export const ChangePassword = props => {
               <small>Password</small>
             </label>
             <input name="password" type="password" value={state.password} onChange={handleChange}/>
+            <div className="text-danger">{state.errors.password}</div>
+          </div>
+          <div>
+            <label htmlFor="confirmPassword">
+              <small>Confirm Password</small>
+            </label>
+            <input name="confirmPassword" type="password" value={state.confirmPassword} onChange={handleChange}/>
+            <div className="text-danger">{state.errors.password}</div>
           </div>
           <button type="submit">Change Credentials</button>
           </form>
