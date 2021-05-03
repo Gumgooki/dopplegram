3c0c55127ddf3cd2548e126c5f75bf1b5c79e6af
Thomas Bunch <17713462+Gumgooki@users.noreply.github.com>
2021-05-03 15:13:35 -0500

    created the like API

---
+++ server/api/like.js
@@ -0,0 +1,34 @@
+const router =  require('express').Router()
+const {Image, Comment, User, Like, Image} = require('../db/models')
+
+router.post('/:imageId/:userId/', async(req, res, next) => {
+  try{
+    const userId = req.params.userId
+    const imageId = req.params.imageId
+    const newLike = req.body.like
+    const like = await Like.create({like: newLike})
+    const image = await Image.findOne({
+      where: {
+        id: imageId
+      },
+    })
+    const user = await User.findOnce({
+      where: {
+        id: userId
+      }
+    })
+    await image.addLike(like)
+    await user.addLike(like)
+    await like.setImage(image)
+    await like.setUser(user)
+
+    //TODO: do we need anything else here? the actual creation of the like should alter the image model to get an actual count
+
+    res.json(like)
+
+  } catch(err){
+    next(err)
+  }
+})
+
+module.exports = router
