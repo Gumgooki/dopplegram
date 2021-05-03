cb9e3e41a8de9efb7df802dbad50d75eefe30bfe
Thomas Bunch <17713462+Gumgooki@users.noreply.github.com>
2021-05-03 15:05:09 -0500

    created a Like model and added some functionality for images to access number of likes without counting the entries on every render.

---
+++ server/db/models/like.js
@@ -0,0 +1,31 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+const Image =  require('./image')
+
+//not sure if this should be boolean or not tbh; i basically just need the ID; if its true it exists, if its false it shouldn't exist
+const Like = db.define('like', {
+  like: {
+    type: Sequelize.BOOLEAN,
+  }
+})
+
+//enabling a way to get an accurate count of how many likes an image has without crawling the whole database everytime we render
+Like.afterUpdate(async like => {
+  const image = await Image.findById(like.imageId)
+  const [data] = await Like.findAll({
+    where: {
+      imageId: like.imageId
+    },
+    attributes: [
+      'imageId',
+      [Sequelize.fn('count', Sequelize.col('imageId')), 'totalLikes']
+    ]
+  })
+  const {totalLikes} = data
+  image.likes = totalLikes
+  await image.save()
+})
+
+module.exports = Like
+
+
