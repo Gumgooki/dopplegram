b9badbd9b020c38dd6728e906e36c21804209244
Thomas Bunch <17713462+Gumgooki@users.noreply.github.com>
2021-06-03 13:32:07 -0500

    testing dif webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,26 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-  entry: '/client/index.js', // assumes your entry point is the index.js in the root of your project folder
-  mode: 'development',
+  entry: "/client/index.js", // assumes your entry point is the index.js in the root of your project folder
+  // mode: 'development',
   output: {
-    path: path.resolve(__dirname, 'public'), // assumes your bundle.js will also be in the root of your project folder
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "public"), // assumes your bundle.js will also be in the root of your project folder
+    filename: "bundle.js",
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
+          loader: "babel-loader",
+        },
       },
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      }
-    ]
-  }
-}
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
